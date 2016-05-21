9f380572b061a2279c5e02cacad0804962d4bf93
sibijr <sibij.r97@gmail.com>
2016-05-21 20:45:59 +0530

    printr

--- staff_details.php
+++ staff_details.php
@@ -8,7 +8,7 @@ $dept = input::get('department');
 $grp1 = '8';
 $result = $db->query_assoc("select name,number,email from users where `department` = '$dept' and `group` = '5';");
 $result = $result->results();
-print_r($result);
+//print_r($result);
 if(isset($result)){
 echo json_encode($result);
 }else{
