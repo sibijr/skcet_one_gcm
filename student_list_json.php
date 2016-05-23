88d75f335f225812e738e3e5586ec599e6ae884a
sibijr <sibij.r97@gmail.com>
2016-05-23 20:27:14 +0530

    echo remove

--- student_list_json.php
+++ student_list_json.php
@@ -4,12 +4,12 @@ require_once('core/init.php');
 if(input::exists()){
 
 $db = DB::getInstance();
-echo $reg_no1 = input::get('reg_no');
+$reg_no1 = input::get('reg_no');
 $result1 = $db->query_assoc("select * from users where `reg_no` = '$reg_no1';");
 $result1 = $result1->first();
-echo $staff_id =  $result1['reg_no'];
-echo $batch = $result1['batch'];
-echo $department = $result1['department'];
+$staff_id =  $result1['reg_no'];
+$batch = $result1['batch'];
+$department = $result1['department'];
 $year = $result1['year'];
 $section = $result1['section'];
 $result = $db->query_assoc("select id,roll_no,name from users where `batch` = '$batch' and `department` = '$department' and `year` = '$year' and `section` = '$section' and `group` = '1';");
